f425394dfad1f4705992eadd335519610c6b6c72
Annitas <deadfoxmaster@mail.ru>
2023-02-05 00:09:09 +0600

    add models for character, location and episode

---
+++ RickAndMortyApp/RickAndMortyApp/Models/RMOrigin.swift
@@ -0,0 +1,13 @@
+//
+//  RMOrigin.swift
+//  RickAndMortyApp
+//
+//  Created by Anita Stashevskaya on 04.02.2023.
+//
+
+import Foundation
+
+struct RMOrigin: Codable {
+    let name: String
+    let url: String
+}
